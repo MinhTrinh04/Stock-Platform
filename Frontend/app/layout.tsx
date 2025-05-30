81fe78ebf33af4ad7b537d31a36fb71e3b61d51b
Minh Trinh <minhfananime@gmail.com>
2025-05-30 23:16:11 +0700

    Fix Hydration Mismatch

--- Frontend/app/layout.tsx
+++ Frontend/app/layout.tsx
@@ -1,18 +1,18 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Finance View',
-}
+  title: "Finance View",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning={true}>
       <body>{children}</body>
     </html>
-  )
+  );
 }
